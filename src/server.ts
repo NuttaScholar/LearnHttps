8e1039420a23205d403b8d8872afa82db4d1df12
NuttaScholar <nutta766@gmail.com>
2025-05-30 23:27:55 +0700

    initial Project

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import https from "https";
+import fs from "fs";
+import path from "path";
+
+// โหลด certificate
+const key = fs.readFileSync(path.join(__dirname, "../cert/key.pem"));
+const cert = fs.readFileSync(path.join(__dirname, "../cert/cert.pem"));
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hello HTTPS with TypeScript");
+});
+
+const server = https.createServer({ key, cert }, app);
+
+const PORT = 4433;
+server.listen(PORT, () => {
+  console.log(`HTTPS Server is running at https://localhost:${PORT}`);
+});
